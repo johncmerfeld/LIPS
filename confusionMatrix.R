1ea28cbc8c399504a425f46e5d8ac11f331496c5
John Merfeld <john.merfeld@gmail.com>
2019-04-25 11:33:51 -0400

    preliminary confusion matrix

---
+++ confusionMatrix.R
@@ -0,0 +1,20 @@
+library(data.table)
+library(matrixStats)
+library(ggplot2)
+
+y <- fread("y.csv")
+y_hat <- fread("y_hat.csv")
+
+y <- toupper(names(y)[max.col(y)])
+y_hat <- toupper(names(y_hat)[max.col(y_hat)])
+
+confusion_matrix <- as.data.frame(table(y_hat, y))
+
+ggplot(data = confusion_matrix,
+       mapping = aes(x = y_hat,
+                     y = y)) +
+  geom_tile(aes(fill = Freq)) +
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "red",
+                      high = "green",
+                      trans = "log")
